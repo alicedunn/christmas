918cb5eacc279a5d0f4a26cacfcb557693cfd6ba
Alice Dunn <alice.dunn@tails.com>
2021-11-26 14:59:19 +0000

    better naming

--- app.py
+++ app.py
@@ -11,10 +11,31 @@ client = lirc.Client()
 def hello_world():
     form = VoteForm()
     if request.method == 'POST':
+        print(request.form)
         response = request.form['vote']
-        if response == 'dog':
+        if response == 'on':
             client.send_once("christmas", "KEY_POWER")
-        if response == 'cat':
+        elif response == 'off':
             client.send_once("christmas", "KEY_POWER2")
+        elif response == 'brighter':
+            client.send_once("christmas", "KEY_BRIGHTNESSUP")
+        elif response == 'dimmer':
+            client.send_once("christmas", "KEY_BRIGHTNESSDOWN")
+        elif response == 'rotate':
+            client.send_once("christmas", "KEY_NUMERIC_1")
+        elif response == 'twinkle':
+            client.send_once("christmas", "KEY_NUMERIC_2")
+        elif response == 'flashing_changes':
+            client.send_once("christmas", "KEY_NUMERIC_3")
+        elif response == 'chill_mode':
+            client.send_once("christmas", "KEY_NUMERIC_4")
+        elif response == 'dancing':
+            client.send_once("christmas", "KEY_NUMERIC_5")
+        elif response == 'slow_fade':
+            client.send_once("christmas", "KEY_NUMERIC_6")
+        elif response == 'party_mode':
+            client.send_once("christmas", "KEY_NUMERIC_7")
+        elif response == 'solid':
+            client.send_once("christmas", "KEY_NUMERIC_8")
 
     return render_template('christmas_vote.html', form=form)
--- form.py
+++ form.py
@@ -2,4 +2,15 @@ from wtforms import Form, RadioField
 
 
 class VoteForm(Form):
-    vote = RadioField('vote', choices=[('dog', 'Dog'), ('cat', 'Cat')])
+    vote = RadioField('vote', choices=[('on', 'On'),
+                                       ('off', 'Off'),
+                                       ('brighter', 'Brighter'),
+                                       ('dimmer', 'Dimmer'),
+                                       ('rotate', 'Rotate Modes'),
+                                       ('twinkle', 'Twinkle'),
+                                       ('flashing_changes', 'Flashing Changes'),
+                                       ('chill_mode', 'Chill Mode'),
+                                       ('dancing', 'Dancing'),
+                                       ('slow_fade', 'Slow Fade'),
+                                       ('party_mode', 'Party Mode'),
+                                       ('solid', 'Solid Light')])
