f17de4f769489259d651582b96f788e741f99360
DanyalGolabi <danyal@tails.com>
2021-11-26 16:16:05 +0000

    wip

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request
-import lirc
+# import lirc
 
 from form import VoteForm
 
 app = Flask(__name__)
-client = lirc.Client()
+# client = lirc.Client()
 
 
 @app.route("/", methods=['POST', 'GET'])
@@ -13,8 +13,10 @@ def hello_world():
     if request.method == 'POST':
         response = request.form['vote']
         if response == 'dog':
-            client.send_once("christmas", "KEY_POWER")
+            # client.send_once("christmas", "KEY_POWER")
+            print('dog')
         if response == 'cat':
-            client.send_once("christmas", "KEY_POWER2")
+            print('cat')
+            # client.send_once("christmas", "KEY_POWER2")
 
     return render_template('christmas_vote.html', form=form)
